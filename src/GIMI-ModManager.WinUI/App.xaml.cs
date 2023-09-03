d6a68c4ada8e8145f3f8f81130afd318a3880277
Jorixon <143650598+Jorixon@users.noreply.github.com>
2023-09-03 17:44:30 +0200

    fix: Removed unecesery code

--- src/GIMI-ModManager.WinUI/App.xaml.cs
+++ src/GIMI-ModManager.WinUI/App.xaml.cs
@@ -21,11 +21,6 @@ namespace GIMI_ModManager.WinUI;
 // To learn more about WinUI 3, see https://docs.microsoft.com/windows/apps/winui/winui3/.
 public partial class App : Application
 {
-    // The .NET Generic Host provides dependency injection, configuration, logging, and other services.
-    // https://docs.microsoft.com/dotnet/core/extensions/generic-host
-    // https://docs.microsoft.com/dotnet/core/extensions/dependency-injection
-    // https://docs.microsoft.com/dotnet/core/extensions/configuration
-    // https://docs.microsoft.com/dotnet/core/extensions/logging
     public IHost Host { get; }
 
     public static T GetService<T>()
