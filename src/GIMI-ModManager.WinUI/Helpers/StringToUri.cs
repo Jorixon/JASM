42bc97005f7da7de30ab8b7ca17d386b7e63cf11
Jorixon <143650598+Jorixon@users.noreply.github.com>
2023-09-09 20:25:26 +0200

    Url button

---
+++ src/GIMI-ModManager.WinUI/Helpers/StringToUri.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml.Data;
+
+namespace GIMI_ModManager.WinUI.Helpers;
+
+public class StringToUri : IValueConverter
+{
+    public object? Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is Uri { IsAbsoluteUri: true } uri)
+            return uri;
+
+        if (value is not string str)
+            return "";
+        Uri.TryCreate(str, UriKind.Absolute, out uri);
+        return uri;
+    }
+
+    public object? ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        return value?.ToString();
+    }
+}
