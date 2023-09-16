31cb03fa8d84b138f16e8fe54bbb7edbcecfa968
Jorixon <143650598+Jorixon@users.noreply.github.com>
2023-09-16 14:24:10 +0200

    mod attention

---
+++ src/GIMI-ModManager.WinUI/Models/Options/ModAttentionSettings.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace GIMI_ModManager.WinUI.Models.Options;
+
+public class ModAttentionSettings
+{
+    [JsonIgnore] public const string Key = "ModAttentionSettings";
+
+    public ModNotification[] ModNotifications { get; set; } = Array.Empty<ModNotification>();
+}
+
+public sealed class ModNotification
+{
+    public string ModCustomName { get; set; }
+    public string ModFolderName { get; set; }
+    public bool ShowOnOverview { get; set; }
+    public AttentionType AttentionType { get; set; }
+    public string Message { get; set; }
+}
+
+public enum AttentionType
+{
+    Added,
+    Modified,
+    UpdateAvailable,
+    Error
+}
