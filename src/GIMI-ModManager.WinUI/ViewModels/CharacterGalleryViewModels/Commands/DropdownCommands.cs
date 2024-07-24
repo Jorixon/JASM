10f4af6ace8f8e5c801104f73e70cb13ac32d3b1
yuukidach <chendamailbox@foxmail.com>
2024-07-25 00:55:04 +0800

    give prompt when deleting

--- src/GIMI-ModManager.WinUI/ViewModels/CharacterGalleryViewModels/Commands/DropdownCommands.cs
+++ src/GIMI-ModManager.WinUI/ViewModels/CharacterGalleryViewModels/Commands/DropdownCommands.cs
@@ -1,6 +1,12 @@
 ﻿using Windows.System;
 using CommunityToolkit.Mvvm.Input;
 using GIMI_ModManager.Core.Helpers;
+using Microsoft.UI.Xaml.Controls;
+using GIMI_ModManager.WinUI.Services.Notifications;
+using Windows.Storage;
+using GIMI_ModManager.WinUI.Services;
+using GIMI_ModManager.WinUI.Contracts.Services;
+using GIMI_ModManager.WinUI.Services.AppManagement;
 
 namespace GIMI_ModManager.WinUI.ViewModels.CharacterGalleryViewModels;
 
@@ -24,14 +30,81 @@ public partial class CharacterGalleryViewModel
         await Launcher.LaunchUriAsync(vm.ModUrl);
     }
 
+    /// <summary>
+    /// return the result of the dialog and if the checkbox "Do not ask again" is checked
+    /// 
+    private async Task<(ContentDialogResult, bool)> PromptDeleteDialog(ModGridItemVm vm)
+    {
+        var windowManager = App.GetService<IWindowManagerService>();
+
+        var doNotAskAgainCheckBox = new CheckBox()
+        {
+            Content = "Do not ask again",
+            IsChecked = false,
+        };
+        var stackPanel = new StackPanel()
+        {
+            Children =
+            {
+                new TextBlock()
+                {
+                    Text = $"Are you sure you want to delete {vm.Name}?",
+                    TextWrapping = Microsoft.UI.Xaml.TextWrapping.WrapWholeWords,
+                },
+                doNotAskAgainCheckBox
+            }
+        };
+
+        var dialog = new ContentDialog()
+        {
+            Title = "Delete mod",
+            Content = stackPanel,
+            PrimaryButtonText = "Delete",
+            CloseButtonText = "Cancel",
+            DefaultButton = ContentDialogButton.Primary,
+        };
+        
+        // get result and check if checkbox is checked
+        var result = await windowManager.ShowDialogAsync(dialog);
+        var doNotAskAgain = doNotAskAgainCheckBox.IsChecked == true;
+        
+        return (result, doNotAskAgain);
+    }
+
     [RelayCommand(CanExecute = nameof(CanOpenModFolder))]
     private async Task DeleteMod(ModGridItemVm vm)
     {
-        if (this._modList is null)
+        if (this._modList is null) {return;}
+
+        var notificationManager = App.GetService<NotificationManager>();
+        var localSettings = App.GetService<ILocalSettingsService>();
+        var doNotShowPrompt = localSettings.ReadSetting<bool>("DoNotShowDeletePromptAgain");
+
+        if (doNotShowPrompt != true)
         {
-            return;
+            var (result, doNotAskAgainChecked) = await PromptDeleteDialog(vm);
+            if (doNotAskAgainChecked)
+            {
+                await localSettings.SaveSettingAsync("DoNotShowDeletePromptAgain", true);
+            }
+
+            if (result != ContentDialogResult.Primary)
+            {
+                return;
+            }
         }
-        this._modList.DeleteModBySkinEntryId(vm.Id);
-        await this.ReloadModsAsync();
+
+        try
+        {
+            this._modList.DeleteModBySkinEntryId(vm.Id);
+            await this.ReloadModsAsync();
+        }
+        catch (Exception e)
+        {
+            _logger.Error(e, "Failed to delete mod");
+            notificationManager.ShowNotification("Failed to delete mod", e.Message, TimeSpan.FromSeconds(10));
+        }
+
+        notificationManager.ShowNotification("Mod deleted", $"{vm.Name} has been deleted", TimeSpan.FromSeconds(5));
     }
 }
