0388d5965a4069586ce2f8fb2a1c0e624089f54d
Jorixon <143650598+Jorixon@users.noreply.github.com>
2025-03-09 14:32:25 +0100

    fix: Pinned characters not being moved to the top on character overview page (#355)
    * Added PostSortAction to add custom sorting of pinned characters
    
    * Updated tests

--- src/GIMI-ModManager.WinUI/Helpers/SortingMethod.cs
+++ src/GIMI-ModManager.WinUI/Helpers/SortingMethod.cs
@@ -10,18 +10,20 @@ public abstract class SortingMethod<T> where T : class
     private readonly T? _firstItem;
 
     public SortingMethod(Sorter<T> sortingMethodType, T? firstItem = null,
-        ICollection<T>? lastItems = null)
+        IEnumerable<T>? lastItems = null)
     {
         _sorter = sortingMethodType;
         _lastItems = lastItems?.ToArray() ?? [];
         _firstItem = firstItem;
     }
 
+    public T? FirstItem => _firstItem;
+
+    public IEnumerable<T> LastItems => _lastItems;
+
     public IEnumerable<T> Sort(IEnumerable<T> items, bool isDescending)
     {
-        IEnumerable<T> sortedItems = null!;
-
-        sortedItems = _sorter.Sort(items, isDescending);
+        var sortedItems = _sorter.Sort(items, isDescending);
 
 
         if (_firstItem is null && _lastItems.Length == 0)
@@ -44,7 +46,14 @@ public abstract class SortingMethod<T> where T : class
             sortedList.Insert(0, _firstItem);
         }
 
+        PostSortAction(sortedList);
+
+
         return sortedList;
+    }
+
+    protected virtual void PostSortAction(List<T> sortedList)
+    {
     }
 
     public override string ToString()
